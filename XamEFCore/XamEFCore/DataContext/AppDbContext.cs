5223af971718c6b7a3069b538ed95785490bcd70
Ventura <marlenp363@gmail.com>
2022-06-22 15:40:22 -0500

    primer commit

---
+++ XamEFCore/XamEFCore/DataContext/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace XamEFCore.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        string DbPath = string.Empty;
+
+        public AppDbContext(string dbPath)
+        {
+            this.DbPath = dbPath;
+        }
+
+        public DbSet<Artista> Artistas { get; set; }
+        public DbSet<Album> Albumes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={DbPath}");
+        }
+    }
+}
